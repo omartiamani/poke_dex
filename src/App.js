e85364807807b3ad70ef92dac8850e7783223d1f
omartiamani <omartiamani@mymate.com>
2019-10-16 00:33:46 +0200

    List of all pokemons + create + remove + search + test + doc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Provider, observer } from "mobx-react";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import Avatar from "@material-ui/core/Avatar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import rootStore from "./store/RootStore";
+import PokemonList from "./components/pokedex/PokemonList";
+import AddForm from "./components/pokedex/AddForm";
+import { POKEBALL_ICON_URL } from "./api/urlConstants";
+
+//Style object that keeps styling related to the component in the same page
+//This styling has been copied from Material-ui site
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  avatar: {
+    margin: 10
+  },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputSearch: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    border: "solid 1px white",
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: 120,
+      "&:focus": {
+        width: 200
+      }
+    }
+  },
+  appBody: {
+    marginLeft: "1%",
+    marginTop: "1%"
+  },
+  addForm: {
+    marginBottom: "1%"
+  }
+});
+
+/**
+ * The App component class.
+ * This is the root React component
+ */
+@observer
+class App extends Component {
+  /**
+   *
+   * @param {Event} event
+   */
+  handleOnSearchInputChange(event) {
+    rootStore.pokedexStore.searchValue = event.target.value;
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { filterPokemonList, searchValue } = rootStore.pokedexStore;
+    return (
+      <Provider rootStore={rootStore}>
+        <AppBar position="sticky">
+          <Toolbar>
+            <Avatar
+              alt="Remy Sharp"
+              src={POKEBALL_ICON_URL}
+              className={classes.avatar}
+            />
+            <Typography className={classes.title} variant="h6" noWrap>
+              Poke App
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                value={searchValue}
+                onChange={this.handleOnSearchInputChange}
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputSearch
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.appBody}>
+          <AddForm className={classes.addForm} />
+          <PokemonList pokemonList={filterPokemonList} />
+        </div>
+      </Provider>
+    );
+  }
 }
 
-export default App;
+//Here we inject the styles object to the component. We can then access it through this.props.classes
+export default withStyles(styles)(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { shallow, mount } from "enzyme";
+import { Provider } from "mobx-react";
+import rootStore from "./store/RootStore";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders correctly", () => {
+  const component = mount(
+    <Provider rootStore={rootStore}>
+      <App />{" "}
+    </Provider>
+  );
+
+  expect(component).toMatchSnapshot();
+  expect(component.find("AddForm")).toHaveLength(1);
+  expect(component.find("PokemonList")).toHaveLength(1);
 });
